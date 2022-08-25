53fa7e2ba217645178d308f88405627ba036224e
sebli <seblewongelabera@gmail.com>
2022-08-25 15:31:24 -0700

    updated file

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -6,27 +6,29 @@ import { deleteLog, setCurrent } from '../../actions/logActions';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
 const LogItem = ({ log, deleteLog, setCurrent }) => {
+  const {id, attention, message, date, tech} = log
   const onDelete = () => {
-    deleteLog(log.id);
+    deleteLog(id);
     M.toast({ html: 'Log Deleted' });
   };
+
   return (
     <li className='collection-item'>
       <div>
         <a
           href='#edit-log-modal'
           className={`modal-trigger ${
-            log.attention ? 'red-text' : 'blue-text'
+            attention ? 'red-text' : 'blue-text'
           }`}
           onClick={() => setCurrent(log)}
         >
-          {log.message}
+          {message}
         </a>
         <br />
         <span className='grey-text'>
-          <span className='black-text'>ID #{log.id}</span> last update by{' '}
-          <span className='black-text'>{log.tech}</span> on{' '}
-          <Moment format='MMMM Do YYYY, h:mm:ss a'></Moment>
+          <span className='black-text'>ID #{id}</span> last update by{' '}
+          <span className='black-text'>{tech}</span> on{' '}
+          <Moment format='MMMM Do YYYY, h:mm:ss a'>{date}</Moment>
         </span>
         <a href='#!' onClick={onDelete} className='secondary-content'>
           <i className='material-icons grey-text'>delete</i>
